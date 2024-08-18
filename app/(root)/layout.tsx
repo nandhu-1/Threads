ad22b2400896fe1b68a4ed0dc27499615cd71eff
nandhu-1 <nandheni.pl@gmail.com>
2024-08-18 20:08:41 +0530

    First commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,42 @@
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Bottombar from "@/components/shared/Bottombar";
+import Leftsidebar from "@/components/shared/Leftsidebar";
+import Rightsidebar from "@/components/shared/Rightsidebar";
+import Topbar from "@/components/shared/Topbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata={
+  title:'Threads',
+  description:'A Nextjs threads application'
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+        <Topbar/>
+        <main className="flex flex-row">
+          <Leftsidebar/>
+          <section className="main-container">
+            <div className="w-full max-w-4xl">
+            {children}
+            </div>
+          </section>
+       <Rightsidebar/>
+        </main>
+        <Bottombar/>
+        </body>
+    </html>
+    </ClerkProvider>
+  );
+}
